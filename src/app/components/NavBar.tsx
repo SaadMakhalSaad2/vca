c99bb20f5be8b995adcc3d83f274faa5e6112583
hommos <hmmos@zip.com>
2024-03-11 19:06:32 -0500

    setup navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <header className="shadow">
+      <div className="mx-auto flex h-14 max-w-5xl items-center justify-between p-3 font-medium">
+        <Link href="/">New Meeting</Link>
+        <SignedIn>
+          <div className="flex items-center gap-5">
+            <Link href="/">Meetings</Link>
+            <UserButton />
+          </div>
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </div>
+    </header>
+  );
+}
