c19637a723bc4e819cfd299fef2a5441c1ad1ea0
hommos <hmmos@zip.com>
2024-03-13 16:11:54 -0500

    mic and cam permissios setup

--- src/app/meeting/[id]/MeetingPage.tsx
+++ src/app/meeting/[id]/MeetingPage.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import PermissionPrompt from "@/app/components/PermissionPrompt";
+import PrimaryButton from "@/app/components/PrimaryButton";
 import useLoadCall from "@/hooks/useLoadCall";
 import useStreamCall from "@/hooks/useStreamCall";
 import { useUser } from "@clerk/nextjs";
 import {
   Call,
   CallControls,
+  DeviceSettings,
   SpeakerLayout,
   StreamCall,
   StreamTheme,
+  VideoPreview,
   useCall,
   useCallStateHooks,
   useStreamVideoClient,
 } from "@stream-io/video-react-sdk";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface MeetingPageProps {
   id: string;
@@ -61,7 +65,7 @@ export default function MeetingPage({ id }: MeetingPageProps) {
   }
   return (
     <StreamCall call={call}>
-      <StreamTheme className="space-y-3">
+      <StreamTheme>
         <MeetingScreen />
       </StreamTheme>
     </StreamCall>
@@ -73,6 +77,15 @@ function MeetingScreen() {
 
   const callEndedAt = useCallEndedAt();
   const callStartsAt = useCallStartsAt();
+  const call = useStreamCall();
+  const description = call.state.custom.description;
+
+  const [setupComplete, setSetupComplete] = useState(false);
+
+  async function handleSetupComplete() {
+    call.join();
+    setSetupComplete(true);
+  }
 
   const futureCall = callStartsAt && new Date(callStartsAt) > new Date();
   if (futureCall) {
@@ -80,11 +93,71 @@ function MeetingScreen() {
   }
 
   const callEnded = !!callEndedAt;
-  // if (callEnded) {
-  //   return <MeetingEndedScreen />;
-  // }
+  if (callEnded) {
+    return <MeetingEndedScreen />;
+  }
 
-  return <div>Call ui</div>;
+  return (
+    <div className="space-y-6">
+      {description && (
+        <p className="text-center">
+          Meeting Description: <span className="font-bold">{description}</span>
+        </p>
+      )}
+      {setupComplete ? (
+        <SpeakerLayout />
+      ) : (
+        <SetupUI onSetupComplete={handleSetupComplete} />
+      )}
+    </div>
+  );
+}
+
+interface SetUpUIProps {
+  onSetupComplete: () => void;
+}
+
+function SetupUI({ onSetupComplete }: SetUpUIProps) {
+  const call = useStreamCall();
+
+  const { useMicrophoneState, useCameraState } = useCallStateHooks();
+
+  const micState = useMicrophoneState();
+  const camState = useCameraState();
+  const [micCamDisabled, setMicCamDisabled] = useState(false);
+
+  useEffect(() => {
+    if (micCamDisabled) {
+      call.camera.disable();
+      call.microphone.disable();
+    } else {
+      call.camera.enable();
+      call.microphone.enable();
+    }
+  }, [micCamDisabled, call]);
+
+  if (!micState.hasBrowserPermission || !camState.hasBrowserPermission) {
+    return <PermissionPrompt />;
+  }
+
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <h1 className="text-center text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center gap-3">
+        <DeviceSettings />
+      </div>
+      <label className="flex items-center gap-2 font-medium">
+        <input
+          type="checkbox"
+          checked={micCamDisabled}
+          onChange={(e) => setMicCamDisabled(e.target.checked)}
+        />
+        Join without mic & camera
+      </label>
+      <PrimaryButton onClick={onSetupComplete}>Join meeting</PrimaryButton>
+    </div>
+  );
 }
 
 function FutureMeetingScreen() {
@@ -106,7 +179,18 @@ function FutureMeetingScreen() {
         </p>
       )}
 
-      <Link className="underline" href="/">Home</Link>
+      <Link className="underline" href="/">
+        Home
+      </Link>
+    </div>
+  );
+}
+
+function MeetingEndedScreen() {
+  return (
+    <div className="flex flex-col items-center gap-6">
+      <p>This meeting has ended</p>
+      <Link href="/" className="underline"></Link>
     </div>
   );
 }
